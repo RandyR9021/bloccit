4070d55cab3d02b85d365c9d2937da0dbae02f49
Randyr902 <rdj7890@gmail.com>
2015-05-24 12:39:28 -0700

    checkpoint-27-crud almost complete

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
