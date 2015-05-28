4c21d76baf3896ad126c2670cec0e9408b285eee
Randyr902 <rdj7890@gmail.com>
2015-05-28 05:53:54 -0700

    fixed master branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   def index
-<<<<<<< HEAD
   	@posts = Post.all
   end
 
   def show
-=======
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
->>>>>>> checkpoint-27-crud
   end
 
   def new
