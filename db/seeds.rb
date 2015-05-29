af47c25bda28cd88d799e0fb5484b29f1f7de6b9
Randyr902 <rdj7890@gmail.com>
2015-05-29 14:36:11 -0700

    checkpoint-38-associations in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
 require 'faker'
 
+# Create Users
+5.times do
+    user = User.new(
+      name:     Faker::Name.name,
+      email:    Faker::Internet.email,
+      password: Faker::Lorem.characters(10)
+    )
+    user.skip_confirmation!
+    user.save!
+end
+users = User.all
+
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
 #use find_or_create_by
 #or use Post.where(title:"unique title").count==0
 
 # Create Posts
 50.times do
     Post.create!(
+      user: users.sample,
       title:  Faker::Lorem.sentence,
       body:   Faker::Lorem.paragraph
     )
@@ -15,6 +36,7 @@ posts = Post.all
 # Create Comments
 100.times do
     Comment.create!(
+       # user: users.sample,   # we have not yet associated Users with Comments              
       post: posts.sample,
       body: Faker::Lorem.paragraph
     )
