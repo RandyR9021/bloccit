5d423d4b93512be7d109f1adf19c5abb70e53328
Randyr902 <rdj7890@gmail.com>
2015-06-01 06:17:04 -0700

    put methods in user.rb class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
-end
-def admin?
-  role == 'admin'
-end
 
-def moderator?
-  role == 'moderator'
-end 
+  def admin?
+		role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end 
+
+end
