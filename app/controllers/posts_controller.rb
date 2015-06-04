6c39f26be6795e0a6df6357b3ed906623e1ecc31
Randyr902 <rdj7890@gmail.com>
2015-06-04 13:01:04 -0700

    updated production environment heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
       @topic = Topic.find(params[:topic_id])
-      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+      @post = current_user.posts.build(post_params)
       authorize @post
       if @post.save
           flash[:notice] = "Post was saved."
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
 
   def update
     @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
+    @post = current_user.posts.build(post_params)
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
@@ -44,3 +44,10 @@ class PostsController < ApplicationController
 
   end
 end 
+
+   private 
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+
+  end 
