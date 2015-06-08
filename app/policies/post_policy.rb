f88d216d09fcddee1bb2e31deaffcebac2d6dec5
Randyr902 <rdj7890@gmail.com>
2015-06-08 12:40:24 -0700

    assignment-39-authorization in progress

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,6 +1,19 @@
 class PostPolicy < ApplicationPolicy
+    class Scope < Scope
+          def resolve
+          if user.admin? || user.moderator?
+            scope.all
+          elsif user.member?
+          scope.where(:user_id => user)
+          end
+          else
+            scope.none
+          end
+        end
+      
 	
 	def index?
 	  true
 	end 
-end
+   end
+ end 
