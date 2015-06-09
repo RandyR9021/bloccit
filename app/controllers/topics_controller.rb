9087c155d2dd856f26913559e85e395380a53ee8
Randyr902 <rdj7890@gmail.com>
2015-06-09 06:01:42 -0700

    fixed topic params on create and update assignment-41-interlude

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,18 +10,18 @@ class TopicsController < ApplicationController
   end
 
   def show
-  	@topic = Topic.find(topic_params)
+  	@topic = Topic.find(params[:id])
     @posts = @topic.posts
   	authorize @topic
   end
 
   def edit
-  	@topic = Topic.find(topic_params)
+  	@topic = Topic.find(params[:id])
   	authorize @topic
   end
   
   def create
-     @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+     @topic = Topic.new(topic_params)
      authorize @topic
      if @topic.save
        redirect_to @topic, notice: "Topic was saved successfully."
@@ -32,9 +32,9 @@ class TopicsController < ApplicationController
    end
  
    def update
-     @topic = Topic.find(topic_params)
+     @topic = Topic.find(params[:id])
      authorize @topic
-     if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+     if @topic.update_attributes(topic_params)
        redirect_to @topic
      else
        flash[:error] = "Error saving topic. Please try again."
@@ -46,5 +46,5 @@ class TopicsController < ApplicationController
 private 
   
   def topic_params
-    params.require(:topic).permit(:title, :body)
+    params.require(:topic).permit(:name, :description, :public)
   end 
