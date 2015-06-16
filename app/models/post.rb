0a71907a8061db0736917b06642e46f1a9b4fda5
Randyr902 <rdj7890@gmail.com>
2015-06-16 14:31:39 -0700

    assignment-post-markdown fixed post

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,7 @@ class Post < ActiveRecord::Base
     validates :topic, presence: true
     validates :user, presence: true
    
-    def markdown_to_html(markdown)
-    	renderer = Redcarpet::Render::HTML.new
-    	extensions = {fenced_code_blocks: true}
-    	redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    	(redcarpet.render markdown).html_safe
+    
 end
 
 		def markdown_title
