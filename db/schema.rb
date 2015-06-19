21e5ff7db9ec242f2d063c567213402f05e6835d
Randyr902 <rdj7890@gmail.com>
2015-06-19 12:13:32 -0700

    fixed spacing and indentation

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,6 @@ ActiveRecord::Schema.define(version: 20150619175900) do
     t.datetime "updated_at",                          null: false
     t.string   "role"
     t.string   "avatar"
-    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
