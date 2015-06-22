f243a459f6a3c9a91536d0aa3b646504367b4c46
Randyr902 <rdj7890@gmail.com>
2015-06-22 12:55:57 -0700

    checkpoint-46-comments completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Bloccit::Application.routes.draw do
+  get 'comments/create'
+
      devise_for :users
-     resources :users, only: [:update]
-     resources :topics do
-     resources :posts, except: [:index]
+      resources :users, only: [:update]
+       resources :topics do
+         resources :posts, except: [:index] do
+           resources :comments, only: [:create]
      end 
-    
+  end  
     get 'about' => 'welcome#about'
      root to: 'welcome#index'
+   end 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -62,4 +66,4 @@ Bloccit::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
