eecf24ec06e6b252123fcea215cd4c24151883ab
Randyr902 <rdj7890@gmail.com>
2015-06-28 12:38:54 -0700

    updated comments assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     @topic = @post.topic
-    @comments = Comment.new
+    @comment = Comment.new
     authorize @comment
   end
 
