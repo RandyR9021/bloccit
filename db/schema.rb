1ea4627ca1d8d01fbae2311e88c9741b7191a917
Randyr902 <rdj7890@gmail.com>
2015-07-02 12:33:29 -0700

    updated master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622181917) do
 ActiveRecord::Schema.define(version: 20150701172143) do
 
   create_table "advertisements", force: :cascade do |t|
@@ -62,6 +61,15 @@ ActiveRecord::Schema.define(version: 20150701172143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "summaries", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
