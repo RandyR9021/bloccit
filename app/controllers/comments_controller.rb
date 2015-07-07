1fd7a89ec44de5951ca58cf084acc5cf5f69e7b0
Randyr902 <rdj7890@gmail.com>
2015-07-07 10:51:57 -0700

    updated comments controller and got everything to pass

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-  	@topic = Topic.find(params[:topic_id])
   	@post = Post.find(params[:post_id])
   	@comment = current_user.comments.build(comment_params)
   	@comment.post = @post
@@ -15,8 +14,7 @@ class CommentsController < ApplicationController
   end 
   	
   def destroy
- 	  @topic = Topic.find(params[:topic_id])
- 	  @post = @topic.posts.find(params[:post_id])
+ 	  @post = Post.find(params[:post_id])
  	  @comment = @post.comments.find(params[:id])
     authorize @comment 
  	  if @comment.destroy
