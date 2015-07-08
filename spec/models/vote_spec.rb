616d1526ed40639ded6481346cfaa0685f9bcb71
Randyr902 <rdj7890@gmail.com>
2015-07-08 13:35:30 -0700

    checkpoint-49-voting completed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
+ require 'rails_helper'
 
-describe Vote do 
-	describe "validations" do
-		describe "value validations" do
-			it "only allows -1 or 1 as values" do
-				expect(up_vote.valid?).to eq(1)
-				expect(down_vote.valid?).to eq(-1)
-			end 
-		end 
-	end 
-end
+ describe Vote do
+   describe "validations" do
+     describe "value validation" do 
+       it "only allows -1 or 1 as values" do 
+         expect(up_vote.valid?).to eq(1)
+         expect(down_vote.valid?).to eq(-1)
+       end
+     end  
+   end
+ end
