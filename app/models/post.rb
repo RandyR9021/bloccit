679dfc2cfee45ec0dab3ac48de84fe10dcd99633
Randyr902 <rdj7890@gmail.com>
2015-07-09 06:23:19 -0700

    assignment-49-voting completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,15 @@ class Post < ActiveRecord::Base
     belongs_to :user
     belongs_to :topic
     mount_uploader :image, ImageUploader
+    after_create :create_vote
 
+    validates :title, length: {minimum: 5}, presence: true
+    validates :body,  length: {minimum: 20}, presence: true
+    # validates :topic, presence: true
+    # validates :user, presence: true
+   
+    default_scope { order('rank DESC') }
+   
     def up_votes 
         votes.where(value: 1).count
     end 
@@ -22,11 +30,13 @@ class Post < ActiveRecord::Base
         new_rank = points + age_in_days
         update_attribute(:rank, new_rank)
     end 
-   
-    default_scope { order('rank DESC') }
     
-    validates :title, length: {minimum: 5}, presence: true
-    validates :body,  length: {minimum: 20}, presence: true
-    # validates :topic, presence: true
-    # validates :user, presence: true
-end
+    
+
+private 
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end 
+end 
+
