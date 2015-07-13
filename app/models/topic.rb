55d528d5596604f631740a41d07aaa7e1a80b617
Randyr902 <rdj7890@gmail.com>
2015-07-13 13:49:42 -0700

    fixed scope view in topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,7 @@ class Topic < ActiveRecord::Base
     validates :name, length: {minimum: 5}, presence: true 
     validates :description, length: {minimum: 20}, presence: true
 
-    scope :visible_to, -> (user) :publicly_viewable
+    scope :visible_to, -> (user) {user ? all : where(public: true)}
     scope :publicly_viewable, -> {user ? all : where(public: true)}
     scope :privately_viewable, -> {user ? all : where(public: false)}
 end
