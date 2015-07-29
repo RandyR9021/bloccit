92665274e3c894ccd57e04aa99d5542a1c6000fe
Randyr902 <rdj7890@gmail.com>
2015-07-29 07:51:21 -0700

    updated assignment-creating-ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,46 +2,44 @@ class CommentsController < ApplicationController
   def create
   	@post = Post.find(params[:post_id])
   	@comments = @post.comments
-  	
+
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.post = @post
     @new_comment = Comment.new
-  	
+
     authorize @comment
-  	
+
     if @comment.save
   		flash[:notice] = "Comment was created successfully."
   	else
   		flash[:error] = "There was an error posting your comment. Please try again."
-  	end 
-    
+  	end
+
     respond_to do |format|
       format.html
       format.js
-    end 
-  end 
-  	
+    end
+  end
+
   def destroy
  	  @post = Post.find(params[:post_id])
  	  @comment = @post.comments.find(params[:id])
-    authorize @comment 
+    authorize @comment
  	  if @comment.destroy
  	  	flash[:notice] = "Comment was removed."
- 	  else 
+ 	  else
  	  	flash[:error] = "Comment couldn't be deleted. Try again."
- 	  end 
+ 	  end
  	respond_to do |format|
     format.html
     format.js
-  end 
-end 
+  end
+end
 
-  private 
-  
+  private
+
   def comment_params
     params.require(:comment).permit(:body)
-  end 
-end 
-
-
+  end
+end
